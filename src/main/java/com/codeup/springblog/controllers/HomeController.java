69c474f3fe446dd1432d449d021b4c6d69591215
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-05-13 12:23:46 -0500

    landing page for controller exercise

---
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.codeup.springblog.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping()
+    @ResponseBody
+    public String landing(){
+        return "This is the landing page!";
+    }
+
+}
